ae2bc766ba2bd723d1ff4b9ec6cf280820558929
Ian M <ian.hg.dev@gmail.com>
2018-04-26 15:12:21 +0900

    A very poor implementation based on ScreenTask

---
+++ LocalTextShareUI.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace LocalTextShare
+{
+    partial class LocalTextShareUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.testTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // testTextBox
+            // 
+            this.testTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.testTextBox.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.testTextBox.Location = new System.Drawing.Point(12, 13);
+            this.testTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.testTextBox.Name = "testTextBox";
+            this.testTextBox.Size = new System.Drawing.Size(839, 26);
+            this.testTextBox.TabIndex = 0;
+            this.testTextBox.Text = "Enter text here";
+            this.testTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TestTextBox_KeyPress);
+            // 
+            // LocalTextShareUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(863, 362);
+            this.Controls.Add(this.testTextBox);
+            this.Font = new System.Drawing.Font("Arial", 9F);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "LocalTextShareUI";
+            this.Text = "Local Text Broadcaster";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox testTextBox;
+    }
+}
+
---
+++ LocalTextShareUI.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+using System.Windows.Forms;
+
+
+namespace LocalTextShare
+{
+    public partial class LocalTextShareUI : Form
+    {
+        private static TraceSource _source = new TraceSource("TestLog");
+        private string _subtitleTemplate = File.ReadAllText(Application.StartupPath + "/WebServer/subtitle_content_template.txt");
+
+        private bool isAcceptingConnections = false;
+        private object syncLock = new object();
+        private ReaderWriterLock rwl = new ReaderWriterLock();
+        HttpListener localServer;
+
+        public LocalTextShareUI()
+        {
+            _source.TraceInformation("Starting up form components");
+            InitializeComponent();
+            StartServer();
+        }
+
+        private async void StartServer()
+        {
+            _source.TraceInformation("Preparing server environment...");
+            await AddFirewallRule();
+            _source.TraceInformation("Starting up server...");
+            localServer = new HttpListener();
+            var url = "http://127.0.0.1:7070/";
+            localServer.Prefixes.Add("http://localhost:7070/");
+            localServer.Prefixes.Add(url);
+            localServer.Start();
+            _source.TraceInformation("Server has started");
+            try
+            {
+                await ServerLoop();
+            }
+            catch (ObjectDisposedException d)
+            {
+                localServer = new HttpListener();
+            }
+            catch (Exception e)
+            {
+                _source.TraceEvent(TraceEventType.Error, 50, e.Message);
+            }
+        }
+
+        private Task AddFirewallRule()
+        {
+            return Task.Run(() =>
+            {
+                string cmd = RunCMD("netsh advfirewall firewall show rule \"Text Broadcast\"");
+                if (cmd.StartsWith("\r\nNo rules match the specified criteria."))
+                {
+                    cmd = RunCMD("netsh advfirewall firewall add rule name=\"Text Broadcast\" dir=in action=allow remoteip=localsubnet protocol=tcp localport=7070");
+                    if (cmd.Contains("Ok."))
+                    {
+                        _source.TraceInformation("Firewall rule \"Text Broadcast\" was added.");
+                    }
+                    else
+                    {
+                        _source.TraceEvent(TraceEventType.Error, 551, cmd);
+                    }
+                }
+                else
+                {
+                    _source.TraceInformation("Firewall rule \"Text Broadcast\" already exists.");
+                }
+            });   
+        }
+
+        private string RunCMD(string cmd)
+        {
+            Process proc = new Process();
+            proc.StartInfo.FileName = "cmd.exe";
+            proc.StartInfo.Arguments = "/C " + cmd;
+            proc.StartInfo.UseShellExecute = false; // This is required to redirect outputs
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.StartInfo.RedirectStandardError = true;
+            proc.Start();
+            string res = proc.StandardOutput.ReadToEnd();
+            proc.StandardOutput.Close();
+
+            proc.Close();
+            return res;
+        }
+
+        private async Task ServerLoop()
+        {
+            isAcceptingConnections = true;
+            while (isAcceptingConnections)
+            {
+                // Wait for next request
+                _source.TraceInformation("Awaiting request...");
+                var ctx = await localServer.GetContextAsync();
+                _source.TraceInformation("Request receieved.");
+                var resPath = ctx.Request.Url.LocalPath;
+                if (resPath.Length <= 1)
+                    resPath = "/index.html";
+                // Get local data as requested
+                var page = Application.StartupPath + "/WebServer" + resPath;
+                if (File.Exists(page))
+                {
+                    byte[] responseData;
+
+                    // Acquire one-time access of HTML file
+                    // TODO: Refactor this into form initialization, index.html shouldn't change
+                    rwl.AcquireReaderLock(Timeout.Infinite);
+                    responseData = File.ReadAllBytes(page);
+                    rwl.ReleaseReaderLock();
+
+                    var fileInfo = new FileInfo(page);
+                    // Define file types for finicky browsers
+                    switch (fileInfo.Extension)
+                    {
+                        case ".css":
+                            ctx.Response.ContentType = "text/css";
+                            break;
+                        case ".html":
+                        case ".htm":
+                            ctx.Response.ContentType = "text/html";
+                            break;
+                    }
+
+                    ctx.Response.StatusCode = 200;
+
+                    try
+                    {
+                        _source.TraceInformation("Sending page data to requester...");
+                        await ctx.Response.OutputStream.WriteAsync(responseData, 0, responseData.Length);
+                    }
+                    catch (Exception e)
+                    {
+                        _source.TraceEvent(TraceEventType.Error, 555, e.Message);
+                    }
+
+                    ctx.Response.Close();
+                    _source.TraceInformation("Response complete.");
+
+                }
+                else
+                {
+                    // Explain (politely) that the WebServer folder is improperly configured.
+                    _source.TraceEvent(TraceEventType.Error, 404, string.Format("Requested page ({0}) not found.", page));
+                    // TODO: Implement
+                }
+            }
+        }
+
+        private void TestTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            _source.TraceInformation(e.KeyChar.ToString());
+            UpdateBroadcastedText(testTextBox.Text);
+        }
+
+        /// <summary>
+        /// Given a string, rewrites a small HTML file that is displayed as an iframe in index.html.
+        /// </summary>
+        /// <param name="myNewText">Next text to display in an iframe in index.html.</param>
+        private void UpdateBroadcastedText(string myNewText)
+        {
+            rwl.AcquireWriterLock(Timeout.Infinite);
+            _source.TraceInformation(myNewText);
+            _source.TraceInformation(_subtitleTemplate);
+            File.WriteAllText(Application.StartupPath + "/WebServer/subtitle_content.html", string.Format(_subtitleTemplate, myNewText, myNewText));
+            rwl.ReleaseWriterLock();
+            
+        }
+    }
+}
